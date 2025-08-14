a85b87b223ff9e3e441ddc5cc8a074399d1dd54b
biruktawit <biruktawitshiferaw522@gmail.com>
2025-08-14 14:53:41 +0300

    Setup PostgreSQL with Prisma Accelerate and seed data

--- prisma/migrations/20250628123509_add_ipfs_hash_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "companies" ADD COLUMN "coverImageIpfsHash" TEXT;
-ALTER TABLE "companies" ADD COLUMN "logoIpfsHash" TEXT;
-
--- AlterTable
-ALTER TABLE "property_images" ADD COLUMN "ipfsHash" TEXT;
